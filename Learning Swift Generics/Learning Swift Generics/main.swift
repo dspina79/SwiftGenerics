63d05e84791c12dba0b8cd6e847348f6351b55a9
Dave Spina <dspina79@gmail.com>
2021-04-29 20:20:10 -0400

    Added some basic examples of generics

--- Learning Swift Generics/Learning Swift Generics/main.swift
+++ Learning Swift Generics/Learning Swift Generics/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
-print("Hello, World!")
+// a very basic generic function
+func count<T>(_ items: [T]) -> Int {
+    return items.count
+}
 
+// T must conform to the numeric protocol
+// this is a generic constraint
+func sumNums<T: Numeric>(_ items: [T]) -> T {
+    return items.reduce(0, +)
+}
+
+
+let items1 = [3,4,5,6,7]
+print("The count is \(count(items1))")
+print("The sum is \(sumNums(items1))")
