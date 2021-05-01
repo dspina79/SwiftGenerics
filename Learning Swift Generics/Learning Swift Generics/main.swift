535a775475758602c1732901e4820208352e6e00
Dave Spina <dspina79@gmail.com>
2021-04-30 20:51:00 -0400

    Added new instances of ggenerics

--- Learning Swift Generics/Learning Swift Generics/main.swift
+++ Learning Swift Generics/Learning Swift Generics/main.swift
@@ -7,6 +7,35 @@
 
 import Foundation
 
+protocol NotificationMessage {
+    var severity: Int { get }
+    func generateNotification()
+}
+
+struct InfoMessage: NotificationMessage {
+    let severity: Int
+    
+    func generateNotification() {
+        if severity > 2 {
+            print("The severity is high")
+        } else {
+            print("Situation normal")
+        }
+    }
+}
+
+struct WarningMessage: NotificationMessage {
+    let severity: Int
+    
+    func generateNotification() {
+        if severity > 2 {
+            print("ALERT ALERT ALERT!")
+        } else {
+            print("Situation normal")
+        }
+    }
+}
+
 // a very basic generic function
 func count<T>(_ items: [T]) -> Int {
     return items.count
@@ -18,7 +47,17 @@ func sumNums<T: Numeric>(_ items: [T]) -> T {
     return items.reduce(0, +)
 }
 
+func handleMessage<T: NotificationMessage>(_ message: T) {
+    message.generateNotification()
+}
+
 
 let items1 = [3,4,5,6,7]
+let items2 = [3.5, 2.3, 2.5, 23]
+let infoMsg = InfoMessage(severity: 3)
+let warningMsg = WarningMessage(severity: 3)
+
 print("The count is \(count(items1))")
 print("The sum is \(sumNums(items1))")
+handleMessage(infoMsg)
+handleMessage(warningMsg)
